002fb92d41787c0648bee43f15005113b1a146cf
Nghia <dtn284@gmail.com>
2025-11-25 20:58:35 +0700

    implement product serrvice

---
+++ backend/apps/product-service/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+
+app.use(
+    cors({
+    origin:["http://localhost:3002","http://localhost:3003"],
+    credentials:true,
+}))
+
+app.listen(8000, () =>{
+    console.log("product service is running on port 8000");
+});
