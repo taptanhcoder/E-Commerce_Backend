4bea9a80efd31321ddc9e3e393496d762351ceed
Nghia <dtn284@gmail.com>
2025-11-26 23:00:55 +0700

    implement

--- backend/apps/order-service/src/index.ts
+++ backend/apps/order-service/src/index.ts
@@ -5,6 +5,14 @@ const fastify = Fastify({
   logger: true
 })
 
+fastify.get ("/health", (request,reply ) =>{
+    return reply.status(200).send({
+    status: "ok",
+    uptime: process.uptime(),
+    timestamp: Date.now(),
+  });
+})
+
 
 const start = async () => {
   try {
