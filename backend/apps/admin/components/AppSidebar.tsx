21089151e74a73f81915862c220ab102191be609
Nghia <dtn284@gmail.com>
2025-12-05 02:18:39 +0700

    commit

--- backend/apps/admin/components/AppSidebar.tsx
+++ backend/apps/admin/components/AppSidebar.tsx
@@ -78,7 +78,7 @@ const AppSidebar = () => {
             <SidebarMenuButton asChild>
               <Link href="/">
                 <Image src="/logo.svg" alt="logo" width={20} height={20} />
-                <span>Lama Dev</span>
+                <span>UIT</span>
               </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
